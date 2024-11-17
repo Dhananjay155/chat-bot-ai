bf9e6121300e557d5b5fe95fcd66763f3244f11f
Dhananjay155 <dhananjaychoudhary017@gmail.com>
2024-11-17 08:51:41 +0530

    added typer page

---
+++ Frontend/src/components/typer/TypingAnim.tsx
@@ -0,0 +1,27 @@
+
+import { TypeAnimation } from "react-type-animation";
+
+const TypingAnim = () => {
+  return (
+    <TypeAnimation
+      sequence={[
+        "Chat with your personalized AI assistant",
+        1000,
+        "Built With OpenAI API 🤖",
+        2000,
+        "Experience your own Customized AI ChatBot 💻",
+        1500,
+      ]}
+      speed={50}
+      style={{
+        fontSize: "60px",
+        color: "white",
+        display: "inline-block",
+        textShadow: "1px 1px 20px #000",
+      }}
+      repeat={Infinity}
+    />
+  );
+};
+
+export default TypingAnim;
